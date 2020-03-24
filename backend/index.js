f1954c2e18c988771e698abf59be5e5a04dd5ee5
Andre Herman Freire Bezerra <amelco.herman@gmail.com>
2020-03-24 16:26:50 -0300

    update README.md

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => {
+    return response.send("Olá, Mundo!");
+});
+
+app.get('/user/:id', (request, response) => {
+    
+    const params = request.params;
+    console.log(params);
+
     return response.json({
         texto: "Olá, mundo!",
-        evento: "Semana omnistack 11.0",
+        evento: "Semana Omnistack 11.0",
         aluno: "Andre Herman"
     });
+});
+
+app.post('/user', (req, res) => {
+    
+    const params = req.body;
+    console.log(params);
+
+    return res.send("Usuário adicionado!")
 });
 
 app.listen(3333);
